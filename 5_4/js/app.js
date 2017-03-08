5935da1c8bcae0dcc5e0668752e60b03b957c4d2
wpiusa <wpiusa@gmail.com>
2017-03-08 14:31:40 -0800

    Add files via upload

---
+++ 5_4/js/app.js
@@ -0,0 +1,8 @@
+var person = [];
+person[0] = ["Tom Lee", 52, 100000,"LA","CA","11111"];
+person[1] = ["Paul Lee", 23, 60000,"LA","CA","11111"];
+person[2] = ["Steve Lee", 22, 70000,"LA","CA","11111"];
+person[3] = ["Sue Lee", 21, 80000,"LA","CA","11111"];
+person[4] = ["Kim Lee", 22, 90000,"LA","CA","11111"];
+
+console.log(person[3]);
